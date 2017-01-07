6358165466a8d062d514a6ac204a3ffca153bc6c
Michael Ghaben <michael@Michaels-MacBook-Pro-9.local>
2017-01-07 14:33:54 -0500

    Basic source and unit test for integration with travis
    Restructure branch
    
    gitignore target repository, fix travis.yml
    
    yml fix

---
+++ src/main/java/module/helloWorld.java
@@ -0,0 +1,13 @@
+package module;
+
+/**
+ * Hello world!
+ *
+ */
+public class helloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
