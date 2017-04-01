5085f9dd0e82dc64c5cddf0590393ec577543ed4
michaelghaben <michaelghaben@users.noreply.github.com>
2017-04-01 16:59:20 -0400

    Trackdevel (#67)
    * Add logging functionality
    
    * rip branch
    
    * Remove launcher, update logger for new names
    
    * Add log examples
    
    * loggy UI added
    
    * block to block functionality
    
    * Update to fix variable scoping
    
    * Updates to the tests, updates to style, updates to build
    
    * Fix formatting to java coding guidelines, add some more updates to blocktoblock
    
    * Update block to support speed and authority messages.

--- src/main/java/TrackModel/Block.java
+++ src/main/java/TrackModel/Block.java
@@ -36,211 +36,223 @@ public class Block implements Comparable<Block>, java.io.Serializable{
   TrackModel superTrackModel;
   Boolean maintenence;
   Boolean hasLight;
-    
+  Double suggestedSpeed;
+  Block authority;
+  Integer trainId;
+
   public Block(TrackModel track, Boolean occupied, Boolean isUnderground, Double blockLen, Double blockGrade, Double elevation, Double speedLimit,
                 String stationName, String arrowDirection, String blockLine, String blockSection, Integer blockNum, Boolean hasSwitch, String switchBlock){
 
-        this.superTrackModel = track;
-        this.blockLen = blockLen;
-        this.blockGrade = blockGrade;
-        this.blockElevation = elevation;
-        this.speedLimit = speedLimit;
-        this.stationName = stationName;
-        this.isUnderground = isUnderground;
-        this.arrowDirection = arrowDirection;
-        this.blockNum = blockNum;
-        this.switchBlock = switchBlock;
-        this.hasSwitch = hasSwitch;
-        this.hasLight = hasSwitch;
-        this.blockLine = blockLine;
-        this.blockSection = blockSection;
+    this.superTrackModel = track;
+    this.blockLen = blockLen;
+    this.blockGrade = blockGrade;
+    this.blockElevation = elevation;
+    this.speedLimit = speedLimit;
+    this.stationName = stationName;
+    this.isUnderground = isUnderground;
+    this.arrowDirection = arrowDirection;
+    this.blockNum = blockNum;
+    this.switchBlock = switchBlock;
+    this.hasSwitch = hasSwitch;
+    this.hasLight = hasSwitch;
+    this.blockLine = blockLine;
+    this.blockSection = blockSection;
 
-        this.lightState=true;
-        this.occupied = false;
-        this.trackHeaters = false;
-        this.signals = false;
-        this.brokenRail = false;
-        this.circuitFailure = false;
-        this.powerFailure = false;
-        this.maintenence=false;
+    this.suggestedSpeed = 0.0;
+    this.lightState=true;
+    this.occupied = false;
+    this.trackHeaters = false;
+    this.signals = false;
+    this.brokenRail = false;
+    this.circuitFailure = false;
+    this.powerFailure = false;
+    this.maintenence = false;
+    this.authority = null;
+    this.trainId = null;
     }
 
-    /**
-    * Returns the length of a block object
-    */
-    public Double getLen(){
-        return this.blockLen;
-    }
+  /**
+  * Returns the broken status of a given block.
+  */
+  public Boolean getBroken(){
+    return this.brokenRail;
+  }
 
-    public Boolean getBroken(){
-        return this.brokenRail;
-    }
+  /**
+  * Setter for the Wayside Controller module to set a light state based upon an Integer
+  * Maps 1 to true, 0 to false based upon the track internal convention.
+  * @param setInt integer for the switch state to be set to
+  * @todo Add strict value assertions that setInt is either 0 or 1 
+  */
+  public Boolean setLightState(Integer setInt){
+    //condition check for having a switch
+    assert(this.lightState != null);
 
-    /**
-    * Setter for the Wayside Controller module to set a light state based upon an Integer
-    * Maps 1 to true, 0 to false based upon the track internal convention.
-    * @param setInt integer for the switch state to be set to
-    * @todo Add strict value assertions that setInt is either 0 or 1 
-    */
-    public Boolean setLightState(Integer setInt){
-        //condition check for having a switch
-        assert(this.lightState != null);
-
-        if(setInt.equals(1)){
-            this.lightState = true;
-        }
-        else if(setInt.equals(0)){
-            this.lightState = false;
-        }
-        return this.lightState;
+    if(setInt.equals(1)){
+      this.lightState = true;
     }
-
-    /**
-    *   Returns if a track block is open or not based upon the state of the breakages of a block
-    */
-    public Boolean getOpen(){
-        if (this.brokenRail || this.powerFailure || this.occupied || this.maintenence){
-            return false;
-        }
-        else{
-            return true;
-        }
+    else if(setInt.equals(0)){
+      this.lightState = false;
     }
+    return this.lightState;
+  }
 
-    /**
-    *   Returns the occupied state of a given block object
-    */
-    public Boolean getOccupied(){
-        return this.occupied;
-    }
+  /**
+  * Returns the length of a block object
+  */
+  public Double getLen(){
+    return this.blockLen;
+  }
 
-    public void setOccupied(){
-        this.occupied= (!this.occupied);
+  /**
+  *   Returns if a track block is open or not based upon the state of the breakages of a block
+  */
+  public Boolean getOpen(){
+    if (this.brokenRail || this.powerFailure || this.occupied || this.maintenence){
+      return false;
     }
+    else{
+      return true;
+    }
+  }
 
-    /**
-    * Returns the speed limit of a given block
-    */
-    public Double getSpeedLimit(){
-        return this.speedLimit;
-    }
+  /**
+  *   Returns the occupied state of a given block object
+  */
+  public Boolean getOccupied(){
+    return this.occupied;
+  }
 
-    /**
-    * Returns the cumulative elevation of a given blcok
-    */
-    public Double getElevation(){
-        return this.blockElevation;
-    }
+  /**
+  * Sets the occupied state of the given block
+  */
+  public void setOccupied(){
+    this.occupied= (!this.occupied);
+  }
 
-    /**
-    * Returns the grade of a given block
-    */
-    public Double getGrade(){
-        return this.blockGrade;
-    }
+  /**
+  * Returns the speed limit of a given block
+  */
+  public Double getSpeedLimit(){
+    return this.speedLimit;
+  }
 
-    public Boolean getCircuitFailure(){
-        return this.circuitFailure;
-    }
+  /**
+  * Returns the cumulative elevation of a given blcok
+  */
+  public Double getElevation(){
+    return this.blockElevation;
+  }
 
-    public Boolean getPowerFailure(){
-        return this.powerFailure;
-    }
+  /**
+  * Returns the grade of a given block
+  */
+  public Double getGrade(){
+    return this.blockGrade;
+  }
 
-    public Boolean getUnderground(){
-        return this.isUnderground;
-    }
+  public Boolean getCircuitFailure(){
+    return this.circuitFailure;
+  }
 
-    public String getStationName(){
-        return this.stationName;
-    }
+  public Boolean getPowerFailure(){
+    return this.powerFailure;
+  }
 
-    public Boolean getTrackHeaters(){
-        return this.trackHeaters;
-    }
+  public Boolean getUnderground(){
+    return this.isUnderground;
+  }
 
-    public void setNextBlockForward(){
-        this.nextBlockForward = this;
-    }
+  public String getStationName(){
+    return this.stationName;
+  }
 
-    public void setNextBlockForward(Block setBlock){
-        this.nextBlockForward = setBlock;
-    }
+  public Boolean getTrackHeaters(){
+    return this.trackHeaters;
+  }
 
-    public Integer getBlockNum(){
-        return this.blockNum;
-    }
+  public void setNextBlockForward(){
+    this.nextBlockForward = this;
+  }
+
+  public void setNextBlockForward(Block setBlock){
+    this.nextBlockForward = setBlock;
+  }
+
+  public Integer getBlockNum(){
+    return this.blockNum;
+  }
     
-    /**
-    * Returns the section a block is on
-    */
-    public String getBlockSection(){
-        return this.blockSection;
-    }
+  /**
+  * Returns the section a block is on
+  */
+  public String getBlockSection(){
+    return this.blockSection;
+  }
 
-    /**
-    * Returns the line a block is on
-    */
-    public String getBlockLine(){
-        return this.blockLine;
-    }
+  /**
+  * Returns the line a block is on
+  */
+  public String getBlockLine(){
+    return this.blockLine;
+  }
 
-    /** Setter for the next block forward in the condition for a switch is present.
-    *   By default, initializes the switch to the lower block (as determined by blockNum)
-    *   to be destination when the this.switchState = true
-    */
-    public void setNextBlockForward(Block lowBlock, Block highBlock){
-        this.nextBlockForward = lowBlock;
-        this.switchNextBlockForward = highBlock;
-        this.switchState = true;
-    }
+  /** Setter for the next block forward in the condition for a switch is present.
+  *   By default, initializes the switch to the lower block (as determined by blockNum)
+  *   to be destination when the this.switchState = true
+  */
+  public void setNextBlockForward(Block lowBlock, Block highBlock){
+    this.nextBlockForward = lowBlock;
+    this.switchNextBlockForward = highBlock;
+    this.switchState = true;
+  }
 
-    /** 
-    * Sets the next block in the "reverse" direction. Does not handle switch conditions
-    * @param setBlock the block to set the default backwards block to
-    */
-    public void setNextBlockBackward(Block setBlock){
-        this.nextBlockBackward = setBlock;
-    }
+  /** 
+  * Sets the next block in the "reverse" direction. Does not handle switch conditions
+  * @param setBlock the block to set the default backwards block to
+  */
+  public void setNextBlockBackward(Block setBlock){
+    this.nextBlockBackward = setBlock;
+  }
 
-    /** 
-    * Sets the root block in the "reverse" direction to deal with switch conditions
-    * @param rootBlock the root block backwards of a switch block
-    */
-    public void setRootBlock(Block rootBlock){
-        this.rootBlock = rootBlock;
-    }
+  /** 
+  * Sets the root block in the "reverse" direction to deal with switch conditions
+  * @param rootBlock the root block backwards of a switch block
+  */
+  public void setRootBlock(Block rootBlock){
+    this.rootBlock = rootBlock;
+  }
 
-    /** Gets the next block forward along based upon the current switch state. If there is no
-    * switch, it returns the next block
-    */
-    public Block nextBlockForward(){
-        if(this.switchState != null){
-            if (this.switchState.equals(true)){
-                return this.nextBlockForward;
-            }
-            else{
-                return this.switchNextBlockForward;
-            }
+  /** Gets the next block forward along based upon the current switch state. If there is no
+  * switch, it returns the next block
+  */
+  public Block nextBlockForward(){
+    if(this.switchState != null){
+      if (this.switchState.equals(true)){
+        return this.nextBlockForward;
         }
-        else{
-            return this.nextBlockForward;
+      else{
+        return this.switchNextBlockForward;
         }
+      }
+    else{
+      return this.nextBlockForward;
     }
+  }
 
-    public Block nextBlockBackward(){
-        if(this.rootBlock != null){
-            if (this.rootBlock.nextBlockForward().equals(this)){
-                return this.rootBlock;
-            }
-            else{               
-                return this;
-            }
+  public Block nextBlockBackward(){
+    if(this.rootBlock != null){
+      if (this.rootBlock.nextBlockForward().equals(this)){
+          return this.rootBlock;
         }
-        else{
-            return this.nextBlockBackward;
+        else{               
+          return this;
         }
+      }
+    else{
+      return this.nextBlockBackward;
     }
+  }
 
     /**
     * Setter for the Wayside Controller module to set a switch state based upon an Integer
